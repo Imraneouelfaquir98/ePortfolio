3d6ee795b4a96ab6a32d0ed3e91993c5cbceb3c4
imraneouelfaquir98 <imrane.ouelfaquir@gmail.com>
2021-07-02 12:07:29 +0100

    My ePortfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+const whoami = ["IMRANE OU EL FAQUIR", "un Programmeur", "un Data Scientist", "un Ingénieur En IA"];
+let index = 0;
+let word = 0;
+const target = document.querySelector("#typing");
+const type = () => {
+	var current = whoami[word];
+
+	if (index++ < current.length) {
+		target.innerText += current[index - 1];
+		setTimeout(type, 111);
+	} else {
+		setTimeout(backspace, 1111);
+	}
+};
+const backspace = () => {
+	if (index > 0) {
+		target.innerText = target.innerText.substr(0, index - 1);
+		index--;
+		setTimeout(backspace, 111);
+	} else {
+		if (word++ == whoami.length - 1) word = 0;
+
+		setTimeout(type, 1111);
+	}
+};
+type();
+
+window.onmousewheel = () => {
+	if (window.scrollY > 200) {
+		document.querySelector("#scroll").classList.add("fadeOut");
+		document.querySelector("#scroll").classList.remove("fadeIn");
+	} else {
+		document.querySelector("#scroll").classList.add("fadeIn");
+		document.querySelector("#scroll").classList.remove("fadeOut");
+	}
+};
